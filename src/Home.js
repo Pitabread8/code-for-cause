596205d9a3810052f4c3020dc3c7d9ee193189ab
ThatCharlieK <86806912+ThatCharlieK@users.noreply.github.com>
2023-12-09 17:10:36 -0800

    Made buttons that navigate from the hompage
    todo: move the stuff from home.js to app.js

--- src/Home.js
+++ src/Home.js
@@ -9,117 +9,192 @@
 // how to install the navigator https://youtu.be/obH0Po_RdWk?si=klORU0qd5ZSoEnF2&t=9018
 
 
-// import * as React from 'react';
-// import {useState} from 'react';
-// import { SafeAreaView, ScrollView, FlatList, StyleSheet, Button, View, Text } from 'react-native';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// import { Video, ResizeMode } from 'expo-av';
-
 
 
 import * as React from 'react';
-import { View, StyleSheet, Button, Text } from 'react-native';
+import { View, StyleSheet, Button, Text, TouchableWithoutFeedback, TouchableOpacity } from 'react-native';
 import { Video } from 'expo-av';
 
+import { FontAwesome5 } from '@expo/vector-icons';
 import PagerView from 'react-native-pager-view';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import QuestionScreen from './Question.js';
+
+
 // import Animated, { useHandler, useEvent } from 'react-native-reanimated';
 
+
+import SolutionsScreen from './Solutions.js';
+
 const styles = StyleSheet.create({
-    pagerView: {
-        flex: 1,
-    },
-    page: {
-        flex: 1,
-    },
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      backgroundColor: '#ecf0f1',
-    },
-    video: {
-      alignSelf: 'center',
-  
-      width: '100%',
-      height: '100%',
-    },
-    buttons: {
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    
-    text: {
-      color: '#fff',
-      fontSize: 30,
-      fontWeight: 'bold'
-    }
-    
+  overlay: {
+    position: 'fixed',
+    right: 0,
+    top: 0,
+    backgroundColor: 'black',
+    color: 'black'
+
+  },
+  pagerView: {
+    flex: 1,
+  },
+  page: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+  },
+  video: {
+    alignSelf: 'center',
+
+    width: '100%',
+    height: '100%',
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  likeButton: {
+    position: 'absolute',
+    bottom: '40%',
+    right: '5%',
+    alignSelf: 'flex-end',
+
+  },
+  solutions: {
+    position: 'absolute',
+    bottom: '30%',
+    right: '7%',
+    alignSelf: 'flex-end',
+
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold'
+  }
+
+});
+
+
+const VideoPlayer = ({ uriIn }) => {
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({
+    shouldPlay: false,
   });
 
-const VideoPlayer = ({uriIn}) => {
-  const video = React.useRef(null);
-  const [status, setStatus] = React.useState({});
+  const navigation = useNavigation();
+
+  const togglePlay = () => {
+    setStatus((prevStatus) => ({
+      ...prevStatus,
+      shouldPlay: !prevStatus.shouldPlay,
+    }));
+  };
+
+  const likeVideo = () => {
+
+    console.log('Icon clicked!');
+  };
+
+  const goToSolutions = () => {
+    navigation.navigate('SolutionsScreen')
+
+  };
+
   return (
-    
+    <View>
+      <TouchableWithoutFeedback onPress={togglePlay}>
+
         <Video
-        ref={video}
-        style={styles.video}
-        source={{
-          uri: uriIn,
-        }}
-        useNativeControls ={false}
-        resizeMode="contain"
-        isLooping
-        shouldPlay
-        onPlaybackStatusUpdate={status => setStatus(() => status)}/>
+          ref={video}
+          style={styles.video}
+          source={{
+            uri: uriIn,
+          }}
+          useNativeControls={false}
+          resizeMode="contain"
+          isLooping
+          shouldPlay={status.shouldPlay}
+          onPlaybackStatusUpdate={(newStatus) => setStatus(() => newStatus)}
+        />
 
-  )
 
-}
-  
+      </TouchableWithoutFeedback>
+      <View style={styles.solutions}>
+        <TouchableOpacity onPress={goToSolutions}>
+          <FontAwesome5 name="lightbulb" size={50} color="black" />
+        </TouchableOpacity>
 
-function pageHasScrolled(){
+      </View>
+
+      <View style={styles.likeButton}>
+        <TouchableOpacity onPress={likeVideo}>
+          <FontAwesome5 name="heart" size={50} color="black" />
+        </TouchableOpacity>
+
+      </View>
+
+    </View>
+  );
+};
+
+
+function pageHasScrolled() {
   console.log("page has Scrolled")
 
 }
-const HomeScreen = ({ navigation }) => {
-    
-    const video2 = React.useRef(null);
-    const [status2, setStatus2] = React.useState({});
-    // const AnimatedPager = Animated.createAnimatedComponent(PagerView);
+const Page = ({ navigation }) => {
+
+  const video2 = React.useRef(null);
+  const [status2, setStatus2] = React.useState({});
+  // const AnimatedPager = Animated.createAnimatedComponent(PagerView);
 
 
-    return (
-       
-        <PagerView style={styles.pagerView} initialPage={0} orientation = "vertical" onPageScroll={pageHasScrolled()}>
-            <View key="1"  style={styles.container}>
+  return (
+    <PagerView style={styles.pagerView} initialPage={0} orientation="vertical" onPageScroll={pageHasScrolled()}>
+      <View key="1" style={styles.container}>
 
-            <View style={styles.container}>
-                <VideoPlayer uriIn = 'https://vod-progressive.akamaized.net/exp=1702159555~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F4046%2F16%2F420234573%2F1814650121.mp4~hmac=8f6cf14f6b84d0cd98e012c631a588330cc6b955fabb4282fbcefff7b3879922/vimeo-prod-skyfire-std-us/01/4046/16/420234573/1814650121.mp4'>
-
-
-                </VideoPlayer>
+        <View style={styles.container}>
+          <VideoPlayer uriIn='https://vod-progressive.akamaized.net/exp=1702167830~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F2176%2F17%2F435882538%2F1900090885.mp4~hmac=2a068126d75ff3b1cc51797157689af3f4a24e15149696e38b48ccba20ec4a23/vimeo-prod-skyfire-std-us/01/2176/17/435882538/1900090885.mp4'>
 
 
-            </View> 
+          </VideoPlayer>
 
 
-            </View>
-            <View key="2">
-              <VideoPlayer uriIn = 'https://vod-progressive.akamaized.net/exp=1702159555~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F4046%2F16%2F420234573%2F1814650121.mp4~hmac=8f6cf14f6b84d0cd98e012c631a588330cc6b955fabb4282fbcefff7b3879922/vimeo-prod-skyfire-std-us/01/4046/16/420234573/1814650121.mp4'>
+        </View>
 
-                </VideoPlayer>
-            </View>
-            </PagerView>
 
-        
-    );
+      </View>
+      <View key="2">
+        <VideoPlayer uriIn='https://vod-progressive.akamaized.net/exp=1702167830~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F2176%2F17%2F435882538%2F1900090885.mp4~hmac=2a068126d75ff3b1cc51797157689af3f4a24e15149696e38b48ccba20ec4a23/vimeo-prod-skyfire-std-us/01/2176/17/435882538/1900090885.mp4'>
+          <Text>Video 2 should go here</Text>
+        </VideoPlayer>
+      </View>
+    </PagerView>
+
+  );
 };
 
-export default HomeScreen;
 
 
 
 
+const Stack = createNativeStackNavigator();
 
+function App() {
+  return (
+    <NavigationContainer independent = {true}>
+      <Stack.Navigator initialRouteName="Home" independent = {true}>
+        <Stack.Screen name="Home" component={Page} />
+        <Stack.Screen name="SolutionsScreen" component={SolutionsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ src/Solutions.js
@@ -0,0 +1,7 @@
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const SolutionsScreen = ({ navigation, route }) => {
+    return <Text>Here are the solutions</Text>;
+};
+
+export default SolutionsScreen;
